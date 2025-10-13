22edebc53e46fc97d7b1892044cad0e74374172d
Colin Pham <ctpham6@dons.usfca.edu>
2025-10-13 02:41:49 -0700

    Empty Home Page Made

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:go_router/go_router.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _home();
+}
+
+class _home extends State<Home> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Stack (
+        children: [
+          Center(
+            child: Text(
+              "Yo",
+              style: TextStyle(
+                color: Colors.black
+              ),
+            )
+          ),
+        ],
+      )
+    );
+  }
+
+}
