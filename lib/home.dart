8f1c7f530a4ced4f42d482cd24e483c957d2f42a
Colin Pham <SettingFire666@gmail.com>
2025-10-14 01:43:15 -0700

    Container and Text

--- lib/home.dart
+++ lib/home.dart
@@ -22,14 +22,40 @@ class _home extends State<Home> {
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: Stack (
         children: [
-          Center(
-            child: Text(
-              "Yo",
-              style: TextStyle(
-                color: Colors.black
-              ),
-            )
-          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 200.0),
+            child: Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    width: 1067.0,
+                    height: 600.0,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 5.0,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: SizedBox()
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Predicted Year",
+                        style: TextStyle(
+                          fontSize: 50.0
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ),
+          )
         ],
       )
     );
